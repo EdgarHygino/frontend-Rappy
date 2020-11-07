fce390413092e5055468114cd9bb8ea4ad045275
EdgarHygino <edgar.hygino@gmail.com>
2020-11-07 10:14:18 -0300

    create page and conect with api

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Landing from './pages/Landing';
-import OrphanegesMap from './pages/OrphanegesMap';
+import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={Landing} ></Route>
-                <Route path='/app' component={OrphanegesMap } ></Route>
+                <Route path='/app' component={OrphanagesMap} ></Route>
+                <Route path='/orphanages/create' component={CreateOrphanage } ></Route>
+                <Route path='/orphanages/:id' component={Orphanage } ></Route>
             </Switch>
         </BrowserRouter>
     );
